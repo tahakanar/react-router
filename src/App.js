e5e8d9d926cb2a858753aab4c753b70146647971
tahakanar <tahakanar@gmail.com>
2022-01-10 19:32:45 +0300

    nested routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import { Route } from "react-router-dom";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import NotFound from "./pages/NotFound";
+import QuoteDetail from "./pages/QuoteDetail";
 
 function App() {
   return (
-    <div>
-      <Route path='/welcome'>
-        <Welcome />
-      </Route>
-      <Route path='/products'>
-        <Products />
-      </Route>
-    </div>
+    <Layout>
+      <Switch>
+        <Route path='/' exact>
+          <Redirect to='/quotes' />
+        </Route>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId'>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote'>
+          <NewQuote />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
